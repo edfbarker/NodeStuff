dc9b59c1674f67115f5629e5775800608f292e01
Ed Barker <edward.f.barker@gmail.com>
2018-09-05 13:04:10 -0400

    modified app.js

---
+++ src/app.js
@@ -0,0 +1,32 @@
+
+// this is being added for feature 2 branch.
+// console.log(module);
+
+const fs = require('fs');   // this line is being changed for feature 2 branch
+
+function processDirList(err, files) {
+    if (err)
+        console.log('Error:', err);
+    else   
+        console.log('File List is ', files)
+
+}
+
+var dir = '/';
+// const files = fs.readdir(dir, processDirList);
+
+// console.log(files);
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+
+function handleLogEvent(eventArg) {
+    console.log(`Handling logging event ${eventArg}`);
+}
+logger.on('messageLogged', handleLogEvent);
+
+logger.log('Original Logging Message');
+
+
+// above line logging cons was removed as part of feature2 branch
